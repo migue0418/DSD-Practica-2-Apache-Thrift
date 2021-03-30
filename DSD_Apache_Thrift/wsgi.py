7e61a83150ce105a8cc5260faaf842deb682aa6e
migue0418 <55736829+migue0418@users.noreply.github.com>
2021-03-30 12:17:17 +0200

    Create Project

---
+++ DSD_Apache_Thrift/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for DSD_Apache_Thrift project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DSD_Apache_Thrift.settings")
+
+application = get_wsgi_application()
